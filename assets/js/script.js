c7e9d7821a6832a9f35b281c2d5d09b1419c82cd
Tazzok <131045020+Tazzok@users.noreply.github.com>
2023-05-31 14:14:31 -0500

    Api searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var main = document.querySelector('#image')
 var apiKey = '05db0d97-ee00-45e4-8ca1-d7ec5749659b';
-var apiParameter = 'object?size=10&sort=random';
+var apiParameter = 'object?size=50&sort=random';
 var apiURL = `https://api.harvardartmuseums.org/${apiParameter}&apikey=${apiKey}`;
 var imageInfo = {
     artistName:'',
-    date:'',
+    classification:'',
+    century:'',
     primaryUrl:'',
     harvUrl:''
 }
@@ -22,13 +23,19 @@ var start = function () {
 
             for (let index = 0; index < data.records.length; index++) {
 
-                var classification = data.records[index].classification;
-                var image = data.records[index].primaryimageurl;
-                if (image) {
+                imageInfo.artistName = data.records[index].people.name;
+                imageInfo.classification = data.records[index].classification;
+                imageInfo.century = data.records[index].century;
+                imageInfo.primaryUrl = data.records[index].primaryimageurl;
+                imageInfo.harvUrl = data.records[index].url;
+                console.log(imageInfo);
+                // var classification = data.records[index].classification;
+                // var image = data.records[index].primaryimageurl;
+                if (imageInfo.primaryUrl) {
                     var imageDisplay = document.createElement('img');
-                    imageDisplay.setAttribute('src', image);
+                    imageDisplay.setAttribute('src', imageInfo.primaryUrl);
                     main.appendChild(imageDisplay);
-                    wikipedia(classification);
+                    wikipedia(imageInfo);
                 }
 
 
@@ -37,8 +44,8 @@ var start = function () {
         })
 };
 
-var wikipedia = function (classification) {
-    var wikiURL = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=${classification}&limit=3&namespace=0&format=json`;
+var wikipedia = function (imageInfo) {
+    var wikiURL = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=${imageInfo.classification}&limit=3&namespace=0&format=json`;
 
     fetch(wikiURL)
         .then(function (response) {
